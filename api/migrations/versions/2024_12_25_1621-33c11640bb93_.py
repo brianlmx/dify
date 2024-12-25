2a27f4b7475e1c063bd429b9b6616421d5e1b1d2
GareArc <chen4851@purdue.edu>
2024-12-25 16:21:54 -0500

    merge heads

---
+++ api/migrations/versions/2024_12_25_1621-33c11640bb93_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: 33c11640bb93
+Revises: d7999dfa4aae, 923752d42eb6
+Create Date: 2024-12-25 16:21:34.769107
+
+"""
+from alembic import op
+import models as models
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '33c11640bb93'
+down_revision = ('d7999dfa4aae', '923752d42eb6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
