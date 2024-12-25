02ec5c9571bbbc15e329469affe88d54d9bfbeef
GareArc <chen4851@purdue.edu>
2024-12-25 16:43:51 -0500

    fix: cannot serilaize error object

--- api/controllers/console/auth/forgot_password.py
+++ api/controllers/console/auth/forgot_password.py
@@ -126,7 +126,7 @@ class ForgotPasswordResetApi(Resource):
             except WorkSpaceNotAllowedCreateError:
                 pass
             except AccountRegisterError as re:
-                return AccountOnRegisterError(description=str(re))
+                raise AccountOnRegisterError(description=str(re))
 
         return {"result": "success"}
 
